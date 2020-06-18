a42fbf699078691c1b1ad638efa3686a6d6590b9
CDinuwan <chanukadinuwan35@gmail.com>
2020-06-18 23:06:41 +0530

    Implicit writing

---
+++ ImplicitTyping/ImplicitTyping/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace ImplicitTyping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "jhon";
+            var lastNmae="Doe";
+
+            var age = 15;
+            int oldAge;
+
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            var stringBuilder = new StringBuilder();
+
+            Console.WriteLine(stringBuilder);
+        }
+    }
+}
