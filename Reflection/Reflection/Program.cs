92e95d78ffcced4b8c538d16994929597e5d69e9
CDinuwan <chanukadinuwan35@gmail.com>
2020-10-21 20:09:01 +0530

    Reflection

---
+++ Reflection/Reflection/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            Console.WriteLine(assembly.FullName);
+
+            var types = assembly.GetTypes();
+            foreach (var type in types)
+            {
+                Console.WriteLine("Type : " + type.Name);
+                var props = type.GetProperties();
+                foreach (var prop in props)
+                {
+                    Console.WriteLine("\tProperty: "+prop.Name+" PropertyType: "+prop.PropertyType);
+                }
+                var methods = type.GetMethods();
+                foreach (var method in methods)
+                {
+                    Console.WriteLine("\tMethod: "+method.Name);
+                }
+            }
+        }
+    }
+    public class Sample
+    {
+        public string Name { get; set; }
+        public int Age;
+        public void MyMethod()
+        {
+
+        }
+    }
+}
