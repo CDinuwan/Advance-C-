1880728dc2b1702ffa202d97dcec24a23a33cdad
CDinuwan <chanukadinuwan35@gmail.com>
2021-03-02 22:33:08 +0530

    Practise implicit typing for advance csharp

---
+++ Implicit/Implicit/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implicit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Chanuka";
+            string lastName;
+
+            int age = 15;
+            int OldAge;
+
+            var Name = "chanuka";//AnyType
+            //Implicit writing variable should be assign when we create it.
+            Console.WriteLine(Name);
+            //Cannot assign null for Implicit type variable.
+            var StringBuilder = new StringBuilder();
+
+            Console.ReadLine();
+        }
+    }
+}
--- Reflection/Reflection/Program.cs
+++ Reflection/Reflection/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
 
 namespace Reflection
@@ -8,32 +9,62 @@ namespace Reflection
         static void Main(string[] args)
         {
             var assembly = Assembly.GetExecutingAssembly();
-            Console.WriteLine(assembly.FullName);
+            //Console.WriteLine(assembly.FullName);
 
-            var types = assembly.GetTypes();
-            foreach (var type in types)
+            //var types = assembly.GetTypes();
+            //foreach (var type in types)
+            //{
+            //    Console.WriteLine("Type : " + type.Name + " Base Type:" + type.BaseType);
+            //    var props = type.GetProperties();
+            //    foreach (var prop in props)
+            //    {
+            //        Console.WriteLine("\tProperty: " + prop.Name + " PropertyType: " + prop.PropertyType);
+            //    }
+            //    var methods = type.GetMethods();
+            //    foreach (var method in methods)
+            //    {
+            //        Console.WriteLine("\tMethod: " + method.Name);
+            //    }
+            //}
+            //var sample = new Sample();
+            //sample.Name = "Jhon";
+            //sample.Age = 24;
+            //var SampleType = typeof(Sample);
+
+            //var nameProperty = SampleType.GetProperty("Name");
+            //Console.WriteLine("Property : "+nameProperty.GetValue(sample));
+
+            //var myMethod = SampleType.GetMethod("MyMethod");
+
+            //myMethod.Invoke(sample, null);
+
+            var typess = assembly.GetTypes().Where(t => t.GetCustomAttributes<MyClassAttribute>().Count() > 0);
+            foreach (var type in typess)
             {
-                Console.WriteLine("Type : " + type.Name);
-                var props = type.GetProperties();
-                foreach (var prop in props)
-                {
-                    Console.WriteLine("\tProperty: "+prop.Name+" PropertyType: "+prop.PropertyType);
-                }
-                var methods = type.GetMethods();
+                Console.WriteLine(type.Name);
+
+                var methods = type.GetMethods().Where(m => m.GetCustomAttributes<MyMethodAttribute> ().Count() > 0);
                 foreach (var method in methods)
                 {
-                    Console.WriteLine("\tMethod: "+method.Name);
+                    Console.WriteLine(method.Name);
                 }
             }
         }
     }
+    [MyClass]
     public class Sample
     {
         public string Name { get; set; }
         public int Age;
+        [MyMethod]
         public void MyMethod()
         {
-
+            Console.WriteLine("Hello from Method");
         }
+        public void NoAttributeMethod() { }
     }
+    [AttributeUsage(AttributeTargets.Class)]
+    public class MyClassAttribute : Attribute { }
+    [AttributeUsage(AttributeTargets.Method)]
+    public class MyMethodAttribute : Attribute { }
 }
