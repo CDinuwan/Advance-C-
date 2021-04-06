b999dab9418b017346b8e45c536ed5dfaaafd031
CDinuwan <chanukadinuwan35@gmail.com>
2021-04-06 13:35:34 +0530

    Object method

---
+++ Generics 1/Generics 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Result
+    {
+        public bool Sucess { get; set; }
+        public int Data { get; set; }
+    }
+}
